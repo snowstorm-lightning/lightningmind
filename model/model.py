0ea349b0506446f2f84af236ab387bfd4331046f
snowstormlightning <snowstormlightning@gmail.com>
2026-02-23 22:36:10 +0800

    finish model implementation

--- model/model.py
+++ model/model.py
@@ -4,7 +4,8 @@ from torch import nn
 import torch.nn.functional as F
 import math
 from transformers.activations import ACT2FN
-
+from transformers import PreTrainedModel, GenerationMixin
+from transformers.modeling_outputs import CausalLMOutputWithPast
 
 class LightningMindConfig(PretrainedConfig):
     model_type = "lightningmind"
@@ -211,3 +212,121 @@ class FeedForward(nn.Module):
         
     def forward(self, x: torch.Tensor):
         return self.down_proj(self.dropout(self.act_fn(self.gate_proj(x)) * self.up_proj(x)))
+    
+class LightningMindBlock(nn.Module):
+    def __init__(self, layer_id: int, config: LightningMindConfig):
+        super().__init__()
+        self.num_attention_heads = config.num_attention_heads
+        self.hidden_size = config.hidden_size
+        self.head_dim = config.hidden_size // config.num_attention_heads
+        self.self_attn = Attention(config)
+        
+        self.layer_id = layer_id
+        self.input_layernorm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        self.post_attention_layernorm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        self.ffn = FeedForward(config)
+        
+    def forward(self, hidden_states: torch.Tensor,
+                positional_embedings: tuple[torch.Tensor, torch.Tensor] | None = None,
+                past_key_value: tuple[torch.Tensor, torch.Tensor]| None = None,
+                use_cache: bool = False,
+                attention_mask: torch.Tensor | None = None):
+        residual = hidden_states
+        hidden_states, present_key_value = self.self_attn(
+            self.input_layernorm(hidden_states), 
+            positional_embedings,
+            past_key_value,
+            use_cache,
+            attention_mask
+        )
+        hidden_states = residual + hidden_states
+        hidden_states = hidden_states + self.ffn(self.post_attention_layernorm(hidden_states))
+        return hidden_states, present_key_value
+    
+class LightningMindModel(nn.Module):
+    def __init__(self, config: LightningMindConfig):
+        super().__init__()
+        self.vocab_size = config.vocab_size
+        self.num_hidden_layers = config.num_hidden_layers
+        self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size)
+        self.dropout = nn.Dropout(config.dropout)
+        self.layers = nn.ModuleList([LightningMindBlock(i, config) for i in range(config.num_hidden_layers)])
+        self.norm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        freq_cos, freq_sin = precompute_freqs_cis(
+            dim=config.hidden_size // config.num_attention_heads,
+            end=config.max_position_embeddings,
+            rope_base=config.rope_theta,
+            rope_scaling=config.rope_scaling
+        )
+        self.register_buffer("freq_cos", freq_cos, persistent=False)
+        self.register_buffer("freq_sin", freq_sin, persistent=False)
+        
+    def forward(self,
+                input_ids: torch.Tensor,
+                attention_mask: torch.Tensor | None = None,
+                use_cache: bool = False,
+                **kwargs):
+        batch_size, seq_len = input_ids.shape
+        if hasattr(past_key_values,'layers'):
+            past_key_values = None
+        past_key_values = past_key_values.layers or [None] * len(self.layers)
+        start_pos = past_key_values[0][0].shape[1] if past_key_values[0] is not None else 0
+        
+        hidden_states = self.dropout(self.embed_tokens(input_ids))
+        
+        positional_embedings = (
+            self.freq_cos[start_pos: start_pos + seq_len],
+            self.freq_sin[start_pos: start_pos + seq_len]
+        )
+        
+        presents = []
+        for layer_idx, (layer, past_key_value) in enumerate(zip(self.layers, past_key_values)):
+            hidden_states, present_key_value = layer(
+                hidden_states,
+                positional_embedings,
+                past_key_value,
+                use_cache,
+                attention_mask
+            )
+            presents.append(present_key_value)
+            
+        hidden_states = self.norm(hidden_states)
+        # aux_loss = sum([l.mlp.aux_loss for l in self.layers if isinstance(l.mlp, MOEFeedForward)], hidden_states.new_zeros(1).squeeze())
+        return hidden_states, presents
+        
+class LightningMindForCausalLM(PreTrainedModel, GenerationMixin):
+    config_class = LightningMindConfig
+    
+    def __init__(self, config: LightningMindConfig):
+        self.config = config or LightningMindConfig()
+        super().__init__(self.config)
+        self.model = LightningMindModel(self.config)
+        self.lm_head = nn.Linear(self.config.hidden_size, self.config.vocab_size, bias=False)
+        self.model.embed_tokens.weight = self.lm_head.weight
+        
+    def forward(self,
+                input_ids: torch.Tensor | None = None,
+                attention_mask: torch.Tensor | None = None,
+                labels: torch.Tensor | None = None,
+                use_cache: bool = False,
+                logits_to_keep: int | torch.Tensor = 0,
+                **args):
+        hidden_states, past_key_values = self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            past_key_values=past_key_values,
+            use_cache=use_cache,
+            **args
+        )
+        slice_indices = slice(-logits_to_keep, None) if isinstance(logits_to_keep, int) else logits_to_keep
+        logits = self.lm_head(hidden_states[:, slice_indices, :])
+
+        loss = None
+        if labels is not None:
+            shift_logits = logits[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            loss = F.cross_entropy(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1), ignore_index=-100)
+
+        output = CausalLMOutputWithPast(loss=loss, logits=logits, past_key_values=past_key_values, hidden_states=hidden_states)
+        # output.aux_loss = aux_loss
+        return output
